1d7cd356268ee149d96966ccd51c28b999196b20
William C <willicar27@gmail.com>
2026-06-22 10:55:49 -0400

    Clases Dominio, Vehiculo, Servicio

---
+++ Dominio/Servicios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Servicios
+    {
+        public string Nombre { get; set; }
+
+        public int PrecioEstandar { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ Dominio/Vehiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Vehiculo
+    {
+        public string Patente { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año  { get; set; }
+
+        public int Kilometro { get; set; }
+
+        public string Motor { get; set; }
+        public string UrlImagen { get; set; }
+        public int IdCliente { get; set; }
+    }
+}
