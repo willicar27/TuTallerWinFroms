a99e75972f149f1f80d6188a8545e0e66531f0ba
William C <willicar27@gmail.com>
2026-06-24 11:12:15 -0400

    creacion clase OrdenTrabajo y DetalleOrden

---
+++ Dominio/DetalleOrden.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class DetalleOrden
+    {
+        public int OrdenTrabajo { get; set; }
+        public int Servicio { get; set; }
+        public String Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public int PrecioUnitario { get; set; }
+    }
+}
---
+++ Dominio/OrdenTrabajo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class OrdenTrabajo
+    {
+        public int ClienteId { get; set; }
+        public int VehiculoId { get; set; }
+        public int MecanicoId { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime HoraInicio { get; set; }
+        public DateTime HoraFin { get; set; }
+        public string Estado { get; set; }
+        public string Total { get; set; }
+    }
+}
